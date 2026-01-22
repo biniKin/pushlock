4e4bd4ad8672fe00d4efd4498355fed22b04a2e5
biniKin <biniyamKinfe21@gmail.com>
2026-01-22 02:43:49 -0800

    identifying opened apps

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
 import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
@@ -24,19 +25,29 @@ class _HomepageState extends State<Homepage> {
     return intalledApps;
   }
 
+  void openAccessibilitySettings(){
+    const intent = AndroidIntent(
+      action: 'android.settings.ACCESSIBILITY_SETTINGS',
+    );
+    intent.launch();
+  }
+
   @override
   void initState() {
     
     super.initState();
+    
+    openAccessibilitySettings();
     ForegroundService.foregroundApps.listen((packageName) {
       print("Opened app: $packageName");
 
       // Example lock condition
       if (packageName == "com.instagram.android") {
         // show lock screen
+        print("...........Instagram is opened................");
       }
     });
-    getApps();
+    // getApps();
   }
 
   @override
@@ -48,11 +59,11 @@ class _HomepageState extends State<Homepage> {
             itemCount: applists.length,
             itemBuilder: (context, index){
               final app = applists[index];
-              print("app name: ${app.name}");
-              print("package name: ${app.packageName}");
-              print("version name: ${app.versionName}");
-              print("version code: ${app.versionCode}");
-              print("installed time: ${app.installedTimestamp}");
+              // print("app name: ${app.name}");
+              // print("package name: ${app.packageName}");
+              // print("version name: ${app.versionName}");
+              // print("version code: ${app.versionCode}");
+              // print("installed time: ${app.installedTimestamp}");
         
               return ListTile(
                 contentPadding: EdgeInsets.all(10),
